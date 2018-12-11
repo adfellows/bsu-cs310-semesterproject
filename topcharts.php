f954b2d60ae08ee004541f7cb47da7724557ce6a
Brayden Gates <44067636+boogates@users.noreply.github.com>
2018-12-10 19:45:57 -0500

    Update topcharts.php

--- topcharts.php
+++ topcharts.php
@@ -10,69 +10,141 @@
 <?php include 'header.php';?>
 <main>
     <br>
-    <div id="Top10">
-        <h1>Top 10 This Week</h1>
+    <div id="Top20">
+        <h1>Top 20 This Week</h1>
         <table id="list">
             <tr>
                 <th>Song</th>
                 <th>Artist</th>
             </tr>
             <tr>
-                <th>1. <a href="https://www.youtube.com/watch?v=DRS_PpOrUZ4">
-                   In My Feelings</th></a>
-                <th>Drake</th>
+                <th>1. <a href="https://www.youtube.com/watch?v=6ONRf7h3Mdk">
+                   SICKO MODE</th></a>
+                <th>Travis Scott</th>
             </tr>
             <tr>
-                <th> 2. <a href="https://www.youtube.com/watch?v=aJOTlE1K90k">
-                   Girls Like You</th></a>
-               <th>Maroon5</th>
+                <th> 2. <a href="https://www.youtube.com/watch?v=gl1aHhXnN1k">
+                   thank you, next</th></a>
+               <th>Ariana Grande</th>
             </tr>
             <tr>
-                <th>3. <a href="https://www.youtube.com/watch?v=xTlNMmZKwpA">
-                   I Like It</th></a>
-                <th>Cardi B, Bad Bunny & J Balvin</th>
+                <th>3. <a href="https://www.youtube.com/watch?v=m7Bc3pLyij0">
+                   Happier</th></a>
+                <th>Marshmello featuring Bastille</th>
             </tr>
             <tr>
-                <th>4. <a href="https://www.youtube.com/watch?v=V_MXGdSBbAI">
-                    FEFE</th></a>
-                <th>6ix9ine Feat. Nicki Minaj & Murda Beatz</th>
+                <th>4. <a href="https://www.youtube.com/watch?v=ZAfAud_M_mg">
+                    Without Me</th></a>
+                <th>Halsey</th>
             </tr>
             <tr>
-                <th>5. <a href="https://www.youtube.com/watch?v=JdkhJhtxFl4">
-                    Better Now</th></a>
-                <th>Post Malone</th>
+                <th>5. <a href="https://www.youtube.com/watch?v=IPXIgEAGe4U">
+                    High Hopes</th></a>
+                <th>Panic! At The Disco</th>
             </tr>
             <tr>
-                <th>6. <a href="https://www.youtube.com/watch?v=mvSItvjFE1c">
-                    Lucky You</th></a>
-                <th>Eminem Feat. Joyner Lucas</th>
+                <th>6. <a href="https://www.youtube.com/watch?v=VWoIpDVkOH0">
+                    Mo Bamba</th></a>
+                <th>Sheck Wes</th>
             </tr>
             <tr>
-                <th>7. <a href="https://www.youtube.com/watch?v=mzB1VGEGcSU">
+                <th>7. <a href="https://www.youtube.com/watch?v=mjaayCARwro">
+                    ZEZE</th></a>
+                <th>Kodak Black featuring Travis Scott</th>
+            </tr>
+            <tr>
+                <th>8. <a href="https://www.youtube.com/watch?v=THcVOf1kNh0">
+                    Drip Too Hard</th></a>
+                <th>Lil Baby & Gunna</th>
+            </tr>
+            <tr>
+                <th>9. <a href="https://www.youtube.com/watch?v=aJOTlE1K90k">
+                    Girls Like You</th></a>
+                <th>Maroon 5 featuring Cardi B</th>
+            </tr>
+            <tr>
+                <th>10. <a href="https://www.youtube.com/watch?v=mzB1VGEGcSU">
                     Lucid Dreams</th></a>
                 <th>Juice WRLD</th>
             </tr>
             <tr>
-                <th>8. <a href="https://www.youtube.com/watch?v=8xhg3V56Aow">
-                    The Ringer</th></a>
-                <th>Eminem</th>
+                <th>11. <a href="https://www.youtube.com/watch?v=UYwF-jdcVjY">
+                    Better Now</th></a>
+                <th>Post Malone</th>
             </tr>
             <tr>
-                <th>9. <a href="https://www.youtube.com/watch?v=d-JBBNg8YKs">
-                    Sicko Mode</th></a>
-                <th>Travis Scott</th>
+                <th>12. <a href="https://www.youtube.com/watch?v=-RJSbO8UZVY">
+                    Youngblood</th></a>
+                <th>5 Seconds Of Summer</th>
             </tr>
             <tr>
-                <th>10. <a href="https://www.youtube.com/watch?v=LjxulQ1bEWg">
-                    Taste</th></a>
-                <th>Tyga Feat. Offset</th>
+                <th>13. <a href="https://www.youtube.com/watch?v=kN0iD0pI3o0">
+                    Breathin</th></a>
+                <th>Ariana Grande</th>
+            </tr>
             <tr>
+                <th>14. <a href="https://www.youtube.com/watch?v=yXQViqx6GMY">
+                    All I Want For Christmas Is You</th></a>
+                <th>Mariah Carey</th>
+            </tr>
+            <tr>
+                <th>15. <a href="https://www.youtube.com/watch?v=U68MJz9DrI4">
+                    Wake Up In The Sky</th></a>
+                <th>Gucci Mane X Bruno Mars X Kodak Black</th>
+            </tr>
+            <tr>
+                <th>16. <a href="https://www.youtube.com/watch?v=W16bk86xIY0">
+                    Trip</th></a>
+                <th>Ella Mai</th>
+            </tr>
+            <tr>
+                <th>17. <a href="https://www.youtube.com/watch?v=ApXoWvfEYVU">
+                    Sunflower</th></a>
+                <th>Post Malone & Swae Lee</th>
+            </tr>
+            <tr>
+                <th>18. <a href="https://www.youtube.com/watch?v=56WBK4ZK_cw">
+                    Eastside</th></a>
+                <th>Benny Blanco, Halsey & Khalid</th>
+            </tr>
+            <tr>
+                <th>19. <a href="https://www.youtube.com/watch?v=OSUxrSe5GbI">
+                    MIA</th></a>
+                <th>Bad Bunny featuring Drake</th>
+            </tr>
+            <tr>
+                <th>20. <a href="https://www.youtube.com/watch?v=xYtsL9znopI">
+                    Love Lies</th></a>
+                <th>Khalid & Normani</th>
+            </tr>
+            <tr>
+                <th>21. <a href="https://www.youtube.com/watch?v=qmbsp-x2E8E">
+                    It's The Most Wonderful Time Of The Year</th></a>
+                <th>Andy Williams</th>
+            </tr>
+            <tr>
+                <th>22. <a href="https://www.youtube.com/watch?v=ixkoVwKQaJg">
+                    Taki Taki</th></a>
+                <th>DJ Snake featuring Selena Gomez, Ozunan and Cardi B</th>
+            </tr>
+            <tr>
+                <th>23. <a href="https://www.youtube.com/watch?v=Idl-K8s1GYo">
+                    Rockin' Around The Christmas Tree</th></a>
+                <th>Brenda Lee</th>
+            </tr>
+            <tr>
+                <th>24. <a href="https://www.youtube.com/watch?v=Zj2cK8wymIA">
+                    Money</th></a>
+                <th>Cardi B</th>
+            </tr>
+            <tr>
+                <th>25. <a href="https://www.youtube.com/watch?v=0I647GU3Jsc">
+                    Natural</th></a>
+                <th>Imagine Dragons</th>
+            </tr>
         </table>
         <br>
     </div>
-    <br>
-        <button type="button" onclick="document.getElementById('list').style.fontSize='35px'">Too Small?</button>
-    <br>
 </main>
 <footer>
 </footer>
